737063565124cdc33d5fa8563fc1b66f11181269
jean202 <hb13pud731@gmail.com>
2023-02-13 15:00:06 +0900

    [feature/jinha-kim/users] feat : OAuth2 관련 코드 작성중

--- server/src/main/java/com/sosim/server/config/SecurityConfig.java
+++ server/src/main/java/com/sosim/server/config/SecurityConfig.java
@@ -1,52 +1,82 @@
 package com.sosim.server.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sosim.server.config.filter.CustomJsonUsernamePasswordAuthenticationFilter;
 import com.sosim.server.config.filter.JwtAuthenticationFilter;
+import com.sosim.server.config.filter.JwtAuthenticationProcessingFilter;
 import com.sosim.server.config.filter.JwtAuthorizationFilter;
+import com.sosim.server.config.handler.AuthFailureHandler;
+import com.sosim.server.config.handler.AuthSuccessHandler;
+import com.sosim.server.config.handler.OAuth2FailureHandler;
+import com.sosim.server.config.handler.OAuth2SuccessHandler;
 import com.sosim.server.repository.UserRepository;
+import com.sosim.server.service.CustomOAuth2UserService;
+import com.sosim.server.service.JwtService;
+import com.sosim.server.service.PrincipalDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserRepository userRepository;
     private final CorsConfig corsConfig;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+//    private final PrincipalOauth2UserService principalOauth2UserService;
+    private final PrincipalDetailsService principalDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        return http
-            .csrf().disable()
+        http
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
+
             .formLogin().disable()
             .httpBasic().disable()
+            .csrf().disable()
+
             .apply(new MyCustomDsl())
             .and()
-            .authorizeRequests(authorize -> authorize
-                .antMatchers("/members/member/**")
-                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-                .antMatchers("/members/member/admin/**")
-                .access("hasRole('ROLE_ADMIN')")
-                // 다른 요청은 다 권한 없이 들어갈 수 있게 설정
-                .anyRequest().permitAll())
-            .build();
+
+            .authorizeRequests()
+            .antMatchers("/members/member/**")
+            .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+            .antMatchers("/members/member/admin/**")
+            .access("hasRole('ROLE_ADMIN')")
+            .anyRequest().permitAll()
+            .and()
+            .oauth2Login()
+            .successHandler(oAuth2SuccessHandler) // 동의하고 계속하기를 눌렀을 때 Handler 설정
+            .failureHandler(oAuth2FailureHandler) // 소셜 로그인 실패 시 핸들러 설정
+            .userInfoEndpoint().userService(customOAuth2UserService);
+
+        http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
+        http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
     public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
@@ -59,4 +89,39 @@ public class SecurityConfig {
                 .addFilter(new JwtAuthorizationFilter(authenticationManager, userRepository));
         }
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(principalDetailsService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public AuthSuccessHandler authSuccessHandler() {
+        return new AuthSuccessHandler(jwtService, userRepository);
+    }
+
+    @Bean
+    public AuthFailureHandler authFailureHandler() {
+        return new AuthFailureHandler();
+    }
+
+    @Bean
+    public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+        CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter
+            = new CustomJsonUsernamePasswordAuthenticationFilter(new ObjectMapper());
+        customJsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        customJsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(authSuccessHandler());
+        customJsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(authFailureHandler());
+        return customJsonUsernamePasswordAuthenticationFilter;
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+        JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter = new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+        return jwtAuthenticationProcessingFilter;
+    }
+
 }
--- server/src/main/java/com/sosim/server/service/PrincipalOauth2UserService.java
+++ server/src/main/java/com/sosim/server/service/PrincipalOauth2UserService.java
@@ -1,74 +1,75 @@
-package com.sosim.server.service;
-
-import com.sosim.server.config.auth.PrincipalDetails;
-import com.sosim.server.config.exception.CustomException;
-import com.sosim.server.domain.dto.response.GoogleUserInfo;
-import com.sosim.server.domain.dto.response.KakaoUserInfo;
-import com.sosim.server.domain.dto.response.NaverUserInfo;
-import com.sosim.server.domain.dto.response.OAuth2UserInfo;
-import com.sosim.server.domain.model.User;
-import com.sosim.server.repository.UserRepository;
-import com.sosim.server.type.ErrorCodeType;
-import com.sosim.server.type.Role;
-import com.sosim.server.type.SocialType;
-import java.util.Map;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
-
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        String socialType = userRequest.getClientRegistration().getRegistrationId();
-
-        OAuth2UserInfo oAuth2UserInfo = null;
-
-        if(socialType.equals(SocialType.KAKAO)) {
-            oAuth2UserInfo = new KakaoUserInfo((Map)((Map)oAuth2User.getAttributes().get("kakao_account")).get("profile"));
-        } else if(socialType.equals(SocialType.NAVER)) {
-            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
-        } else if(socialType.equals(SocialType.GOOGLE)) {
-            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
-        } else {
-            throw new CustomException(ErrorCodeType.PROVIDER_LIST);
-        }
-
-        String socialId = oAuth2UserInfo.getProviderId();
-        String nickname = socialType + "_" + socialId;
-        String password = passwordEncoder.encode("samplePassword");
-        String email = oAuth2UserInfo.getEmail();
-        Role role = Role.USER;
-
-        if (userRepository.findByEmail(email).isPresent()) {
-            throw new CustomException(ErrorCodeType.USER_EMAIL_ALREADY_EXIST, userRepository.findByEmail(email).orElseThrow(() -> new CustomException(ErrorCodeType.NOT_FOUND_EMAIL)).getEmail());
-        }
-
-        if (userRepository.findByNickname(nickname).isPresent()) {
-            throw new CustomException(ErrorCodeType.USER_NICKNAME_ALREADY_EXIST, userRepository.findByEmail(nickname).orElseThrow(() -> new CustomException(ErrorCodeType.NOT_FOUND_NICKNAME)).getNickname());
-        }
-
-        User userEntity = User.builder()
-            .nickname(nickname)
-            .password(password)
-            .email(email)
-            .role(role)
-            .socialType(SocialType.valueOf(socialType))
-            .socialId(socialId)
-            .build();
-        userRepository.save(userEntity);
-
-        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
-    }
-}
+//package com.sosim.server.service;
+//
+//import com.sosim.server.config.auth.PrincipalDetails;
+//import com.sosim.server.config.exception.CustomException;
+//import com.sosim.server.domain.dto.response.GoogleUserInfo;
+//import com.sosim.server.domain.dto.response.KakaoUserInfo;
+//import com.sosim.server.domain.dto.response.NaverUserInfo;
+//import com.sosim.server.domain.dto.response.OAuth2UserInfo;
+//import com.sosim.server.domain.model.User;
+//import com.sosim.server.repository.UserRepository;
+//import com.sosim.server.type.ErrorCodeType;
+//import com.sosim.server.type.Role;
+//import com.sosim.server.type.SocialType;
+//import java.util.Map;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+//
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+//
+//        OAuth2User oAuth2User = super.loadUser(userRequest);
+//        String socialType = userRequest.getClientRegistration().getRegistrationId();
+//
+//        OAuth2UserInfo oAuth2UserInfo = null;
+//
+//        if(socialType.equals(SocialType.KAKAO)) {
+//            oAuth2UserInfo = new KakaoUserInfo((Map)((Map)oAuth2User.getAttributes().get("kakao_account")).get("profile"));
+//        } else if(socialType.equals(SocialType.NAVER)) {
+//            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+//        } else if(socialType.equals(SocialType.GOOGLE)) {
+//            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+//        } else {
+//            throw new CustomException(ErrorCodeType.PROVIDER_LIST);
+//        }
+//
+//        String socialId = oAuth2UserInfo.getProviderId();
+//        String nickname = socialType + "_" + socialId;
+//        // TODO 이부분 의문 해결
+//        String password = passwordEncoder.encode("samplePassword");
+//        String email = oAuth2UserInfo.getEmail();
+//        Role role = Role.USER;
+//
+//        if (userRepository.findByEmail(email).isPresent()) {
+//            throw new CustomException(ErrorCodeType.USER_EMAIL_ALREADY_EXIST, userRepository.findByEmail(email).orElseThrow(() -> new CustomException(ErrorCodeType.NOT_FOUND_EMAIL)).getEmail());
+//        }
+//
+//        if (userRepository.findByNickname(nickname).isPresent()) {
+//            throw new CustomException(ErrorCodeType.USER_NICKNAME_ALREADY_EXIST, userRepository.findByEmail(nickname).orElseThrow(() -> new CustomException(ErrorCodeType.NOT_FOUND_NICKNAME)).getNickname());
+//        }
+//
+//        User userEntity = User.builder()
+//            .nickname(nickname)
+//            .password(password)
+//            .email(email)
+//            .role(role)
+//            .socialType(SocialType.valueOf(socialType))
+//            .socialId(socialId)
+//            .build();
+//        userRepository.save(userEntity);
+//
+//        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
+//    }
+//}
